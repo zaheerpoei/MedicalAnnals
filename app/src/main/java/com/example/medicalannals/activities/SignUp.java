7fce6e2718e91323a5f8bf636ea0dcf0ade8b6bf
zaheer.ahmed <zaheer.ahmed@appinsnap.com>
2022-02-09 10:33:06 +0500

    sign in and sign up with firebase

--- app/src/main/java/com/example/medicalannals/activities/SignUp.java
+++ app/src/main/java/com/example/medicalannals/activities/SignUp.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.accessibilityservice.AccessibilityGestureEvent;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -18,9 +20,13 @@ import android.widget.Toast;
 
 import com.example.medicalannals.R;
 import com.example.medicalannals.models.PatientModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,15 +37,18 @@ import com.google.firebase.database.ValueEventListener;
 public class SignUp extends AppCompatActivity {
 
     TextView tvSignInForAnAccount;
-    TextInputEditText tiedUsernameSignUp , tiedPasswordSignUp,tiedConfirmPasswordSignUp, tiedContactSignUp, tiedAgeSignUp, tiedGenderSignUp , tiedExperienceSignUp , tiedSpecializationSignUp, tiedFeeSignUp , tiedQualification;
-    String  stUsernameSignUp , stPasswordSignUp, stConfirmPasswordSignUp, stContactSignUp,stAgeSignUp, stGenderSignUp , stSpecializationSignUp , stExperienceSignUp;
+    TextInputEditText tiedUsernameSignUp, tiedPasswordSignUp, tiedConfirmPasswordSignUp, tiedContactSignUp, tiedAgeSignUp, tiedGenderSignUp, tiedExperienceSignUp, tiedSpecializationSignUp, tiedFeeSignUp, tiedQualification;
+    String stUsernameSignUp, stPasswordSignUp, stConfirmPasswordSignUp, stContactSignUp, stAgeSignUp, stGenderSignUp, stSpecializationSignUp, stExperienceSignUp;
     FirebaseAuth mAuth;
     ConstraintLayout constraintLayout;
     Button btnSignUp;
-    RadioButton rbDoctorSignUp , rbPatientSignUp;
-    TextInputLayout tiExperienceSignUp , spinnerSpecialization , tiFeeSignUp , spinnerHospital , tiQualification;
-    String[] type = new String[] {"Child Specialist", "Skin Specialist", "Psychiatrist", "Diabetes Specialist","Eye Specialist", "Dentist" , "ENT Specialist", "Kidney Specialist" , "Heart Specialist" , "Gastroenterologist"};
-    String[] typeHospital = new String[] {"KRL Hospital", "Shifa International Hospital", "Bilal Hospital", "Ahmed Medical Complex","Holy Family"};
+
+    ProgressDialog progressDialog;
+    RadioButton rbDoctorSignUp, rbPatientSignUp;
+    TextInputLayout tiExperienceSignUp, spinnerSpecialization, tiFeeSignUp, spinnerHospital, tiQualification;
+    String[] type = new String[]{"Child Specialist", "Skin Specialist", "Psychiatrist", "Diabetes Specialist", "Eye Specialist", "Dentist", "ENT Specialist", "Kidney Specialist", "Heart Specialist", "Gastroenterologist"};
+    String[] typeHospital = new String[]{"KRL Hospital", "Shifa International Hospital", "Bilal Hospital", "Ahmed Medical Complex", "Holy Family"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,42 +59,42 @@ public class SignUp extends AppCompatActivity {
         clickListeners();
         setSpinners();
 
-        PatientModel patientModel = new PatientModel("Adnan","zaheer.ahmed@appinsnap.com","Male",29,"03246159187");
-
-
-        String uid="Adnan";
-        FirebaseDatabase database=FirebaseDatabase.getInstance();
-        DatabaseReference reference=database.getReference("Patient");
-
-        reference.child(uid).setValue(patientModel);
-//        reference.child(uid).child( "Email" ).setValue("Email");
-//        reference.child(uid).child( "Contact" ).setValue("Contact");
-//        reference.child(uid).child( "Id" ).setValue(uid);
-//        reference.child(uid).child( "Age" ).setValue("Age");
-//        reference.child(uid).child( "Department" ).setValue("Department");
-//        activity.startActivity(new Intent(activity, MainActivity.class));
-
-        DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
-        dref.child("Users").child("zaheer").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-//                    PatientModel patientModel1 = dataSnapshot.getValue(PatientModel.class);
-//                    Toast.makeText(getApplicationContext(), patientModel1.getName(), Toast.LENGTH_LONG).show();
-
-                    String name = dataSnapshot.child("Name").getValue().toString();
-                    Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
-
-                } else {
-                    Toast.makeText(getApplicationContext(), "No notice found.", Toast.LENGTH_LONG).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+//        PatientModel patientModel = new PatientModel("Adnan", "zaheer.ahmed@appinsnap.com", "Male", 29, "03246159187");
+//
+//
+//        String uid = "Adnan";
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference reference = database.getReference("Patient");
+//
+//        reference.child(uid).setValue(patientModel);
+////        reference.child(uid).child( "Email" ).setValue("Email");
+////        reference.child(uid).child( "Contact" ).setValue("Contact");
+////        reference.child(uid).child( "Id" ).setValue(uid);
+////        reference.child(uid).child( "Age" ).setValue("Age");
+////        reference.child(uid).child( "Department" ).setValue("Department");
+////        activity.startActivity(new Intent(activity, MainActivity.class));
+//
+//        DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
+//        dref.child("Users").child("zaheer").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.exists()) {
+////                    PatientModel patientModel1 = dataSnapshot.getValue(PatientModel.class);
+////                    Toast.makeText(getApplicationContext(), patientModel1.getName(), Toast.LENGTH_LONG).show();
+//
+//                    String name = dataSnapshot.child("Name").getValue().toString();
+//                    Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+//
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "No notice found.", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
     }
 
     private void setSpinners() {
@@ -113,7 +122,7 @@ public class SignUp extends AppCompatActivity {
     }
 
     private void visibility() {
-        if(rbPatientSignUp.isChecked()== true){
+        if (rbPatientSignUp.isChecked() == true) {
             tiedExperienceSignUp.setVisibility(View.GONE);
 //            tiedSpecializationSignUp.setVisibility(View.GONE);
             tiExperienceSignUp.setVisibility(View.GONE);
@@ -123,8 +132,7 @@ public class SignUp extends AppCompatActivity {
             tiFeeSignUp.setVisibility(View.GONE);
             tiedQualification.setVisibility(View.GONE);
             tiQualification.setVisibility(View.GONE);
-        }
-        else if(rbDoctorSignUp.isChecked()== true){
+        } else if (rbDoctorSignUp.isChecked() == true) {
             tiedExperienceSignUp.setVisibility(View.VISIBLE);
 //            tiedSpecializationSignUp.setVisibility(View.VISIBLE);
             tiExperienceSignUp.setVisibility(View.VISIBLE);
@@ -154,9 +162,13 @@ public class SignUp extends AppCompatActivity {
         tiFeeSignUp = findViewById(R.id.ti_fee_sign_up);
         spinnerHospital = findViewById(R.id.spinner_hospital);
         tiedQualification = findViewById(R.id.tied_qualification_sign_up);
-        tiQualification  = findViewById(R.id.ti_qualification_sign_up);
+        tiQualification = findViewById(R.id.ti_qualification_sign_up);
 
         btnSignUp = findViewById(R.id.btn_sign_up);
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering..... ");
     }
 
     private void clickListeners() {
@@ -164,7 +176,7 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(SignUp.this , SignIn.class);
+                Intent i = new Intent(SignUp.this, SignIn.class);
                 startActivity(i);
 
             }
@@ -172,12 +184,16 @@ public class SignUp extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String number = tiedContactSignUp.getText().toString();
-
-                Intent i = new Intent(SignUp.this , AuthenticateUser.class);
-                i.putExtra("PhoneNumber" , number);
-                startActivity(i);
+//                checkFields();
+                progressDialog.show();
+                PatientModel patientModel = new PatientModel("Ibra", "ibra.noor@appinsnap.com", "Female", 25, "03425582536");
+                RegisterPatient(patientModel, "ibra.noor");
+//                RegisterPatient(patientModel,stPasswordSignUp);
+//                String number = tiedContactSignUp.getText().toString();
+//
+//                Intent i = new Intent(SignUp.this , AuthenticateUser.class);
+//                i.putExtra("PhoneNumber" , number);
+//                startActivity(i);
 
             }
         });
@@ -212,8 +228,7 @@ public class SignUp extends AppCompatActivity {
 
     }
 
-    private boolean checkFields()
-    {
+    private boolean checkFields() {
         tiedUsernameSignUp.setError(null);
         tiedPasswordSignUp.setError(null);
         tiedConfirmPasswordSignUp.setError(null);
@@ -238,61 +253,49 @@ public class SignUp extends AppCompatActivity {
             snackbar.show();
             focusView = tiedUsernameSignUp;
             cancel = true;
-        }
-
-        else if (TextUtils.isEmpty(stPasswordSignUp)) {
+        } else if (TextUtils.isEmpty(stPasswordSignUp)) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Enter Password", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedPasswordSignUp;
             cancel = true;
-        }
-        else if (tiedPasswordSignUp.length() < 8) {
+        } else if (tiedPasswordSignUp.length() < 8) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Password too short", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedPasswordSignUp;
             cancel = true;
-        }
-
-        else if (TextUtils.isEmpty(stConfirmPasswordSignUp)) {
+        } else if (TextUtils.isEmpty(stConfirmPasswordSignUp)) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Enter Confirm Password", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedConfirmPasswordSignUp;
             cancel = true;
-        }
-        else if (tiedConfirmPasswordSignUp.length() < 8) {
+        } else if (tiedConfirmPasswordSignUp.length() < 8) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Password too short", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedConfirmPasswordSignUp;
             cancel = true;
-        }
-        else if (tiedContactSignUp.length() < 8) {
+        } else if (tiedContactSignUp.length() < 8) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Enter Contact Number", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedContactSignUp;
             cancel = true;
-        }
-        else if (tiedAgeSignUp.length() < 8) {
+        } else if (tiedAgeSignUp.length() < 8) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Enter Age", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedAgeSignUp;
             cancel = true;
-        }
-
-        else if (tiedGenderSignUp.length() < 8) {
+        } else if (tiedGenderSignUp.length() < 8) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Enter Gender", Snackbar.LENGTH_LONG);
             snackbar.show();
             focusView = tiedGenderSignUp;
             cancel = true;
-        }
-
-        else if (tiedExperienceSignUp.length() < 8) {
+        } else if (tiedExperienceSignUp.length() < 8) {
             Snackbar snackbar = Snackbar
                     .make(constraintLayout, "Enter Experience", Snackbar.LENGTH_LONG);
             snackbar.show();
@@ -311,8 +314,7 @@ public class SignUp extends AppCompatActivity {
 
     }
 
-    private void authenticateUser()
-    {
+    private void authenticateUser() {
 /*        mAuth.createUserWithEmailAndPassword(stEmailAddressSignUp,stPasswordSignUp).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -326,5 +328,43 @@ public class SignUp extends AppCompatActivity {
                 }
             }
         });*/
+    }
+
+    public void RegisterPatient(PatientModel patientModel, String Password) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(patientModel.getEmail(), Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                            DatabaseReference reference = database.getReference("Patient");
+                            reference.child(uid).setValue(patientModel);
+
+                            Intent intent = new Intent(SignUp.this, PatientDashboard.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+//                            activity.startActivity(new Intent(activity, MainActivity.class));
+//                            Toast.makeText(activity, "Account Created", Toast.LENGTH_SHORT).show();
+//                            activity.finish();
+//                            progressDialog.dismiss();
+
+
+                        }else {
+                            Toast.makeText(SignUp.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                progressDialog.dismiss();
+                Toast.makeText(SignUp.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        });
     }
 }
