c02f2732b7a3e4b765a19a660e46790469e8f2f0
ibra.noor <ibra.noor@appinsnap.com>
2022-04-18 12:36:24 +0500

    18-04-2022 textPassword

--- app/src/main/java/com/example/medicalannals/activities/ProfileManagement.java
+++ app/src/main/java/com/example/medicalannals/activities/ProfileManagement.java
@@ -1,16 +1,32 @@
 package com.example.medicalannals.activities;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.medicalannals.R;
 import com.example.medicalannals.models.PatientModel;
+
+import java.net.URI;
 
 public class ProfileManagement extends AppCompatActivity {
 
@@ -18,18 +34,40 @@ public class ProfileManagement extends AppCompatActivity {
     private EditText etFullName,etAddress,etPhoneNumber;
     PatientModel patientModel;
     ConstraintLayout clChangePassword;
+    TextView tvChangePatient;
+    Uri sIUActionGetContent;
+    ImageView ivPatientPic;
+    //these two below lines
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    SharedPreferences sharedpreferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_management);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
         Intent i = getIntent();
         patientModel = (PatientModel)i.getSerializableExtra("patientModel");
         initViews();
-
         clickListeners();
+        Uri getUri = Uri.parse(sharedpreferences.getString("sIUActionGetContent", ""));
+        //this if else statement
+        if (sharedpreferences.contains("sIUActionGetContent")) {
+//            Toast.makeText(ProfileManagement.this, "Image  selected!", Toast.LENGTH_SHORT).show();
+            if (null != getUri) {
+                ivPatientPic.setImageURI(getUri);
+            } else {
+                Toast.makeText(ProfileManagement.this, "Image not selected!", Toast.LENGTH_SHORT).show();
+            }
+//            if (null != sIUActionGetContent) {
+
+//                ivPatientPic.setImageURI(getUri);
+//            } else {
+//                Toast.makeText(ProfileManagement.this, "Image not selected!", Toast.LENGTH_SHORT).show();
+//            }
+        }
     }
 
     private void initViews() {
@@ -39,6 +77,9 @@ public class ProfileManagement extends AppCompatActivity {
         etFullName = findViewById(R.id.ed_full_name_patient);
         etAddress = findViewById(R.id.ed_email_address_patient);
         etPhoneNumber = findViewById(R.id.ed_phone_number_patient);
+        tvChangePatient = findViewById(R.id.tv_change_patient);
+        ivPatientPic = findViewById(R.id.iv_patient_pic);
+
         etFullName.setText(patientModel.getName());
         etAddress.setText(patientModel.getEmail());
         etPhoneNumber.setText(patientModel.getNumber());
@@ -61,5 +102,58 @@ public class ProfileManagement extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        tvChangePatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(ProfileManagement.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(ProfileManagement.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        Toast.makeText(ProfileManagement.this, "Please Accept Permission", Toast.LENGTH_LONG).show();
+                    } else {
+                        ActivityCompat.requestPermissions(ProfileManagement.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    }
+                }
+                imageChooserActionGetContent();
+            }
+        });
     }
+
+    void imageChooserActionGetContent() {
+        Intent i = new Intent();
+        i.setType("image/*");
+//        i.putExtra(i.EXTRA_ALLOW_MULTIPLE,true);    //for picking multiple images
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        activityResultLauncher.launch(i);
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == RESULT_OK) {
+                assert result.getData() != null;
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                sIUActionGetContent = result.getData().getData();
+                //these two
+                Log.d("sIUActionGetContent", sIUActionGetContent.toString());
+                editor.putString("sIUActionGetContent", sIUActionGetContent.toString());
+                editor.commit();
+                //this
+                String getUri = sharedpreferences.getString("sIUActionGetContent", "");
+                //this if else statement
+//                if (getUri.contains("sIUActionGetContent")) {
+//                    if (null != sIUActionGetContent) {
+//                        ivPatientPic.setImageURI(sIUActionGetContent);
+//                    } else {
+//                        Toast.makeText(ProfileManagement.this, "Image not selected!", Toast.LENGTH_SHORT).show();
+//                    }
+//                } else {
+                    if (null != sIUActionGetContent) {
+                        ivPatientPic.setImageURI(sIUActionGetContent);
+                    } else {
+                        Toast.makeText(ProfileManagement.this, "Image not selected!", Toast.LENGTH_SHORT).show();
+                    }
+//                }
+            }
+        }
+    });
 }
--- app/src/main/java/com/example/medicalannals/adapters/ViewSlotsAdapter.java
+++ app/src/main/java/com/example/medicalannals/adapters/ViewSlotsAdapter.java
@@ -1,12 +1,14 @@
 package com.example.medicalannals.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.medicalannals.R;
@@ -19,6 +21,7 @@ public class ViewSlotsAdapter extends RecyclerView.Adapter<ViewSlotsAdapter.MyVi
     private ArrayList<ViewSlotsModel> viewSlotsModelArrayList;
     private Context mcontext;
     private SelectedDate selectedDate;
+    public static int selected_position = -1;
 
     public ViewSlotsAdapter(ArrayList<ViewSlotsModel> viewSlotsModelArrayList, Context mcontext,SelectedDate selectedDate) {
         this.viewSlotsModelArrayList = viewSlotsModelArrayList;
@@ -41,12 +44,21 @@ public class ViewSlotsAdapter extends RecyclerView.Adapter<ViewSlotsAdapter.MyVi
         String[] date = viewSlotsModel.getDate().split("-");
         holder.tvDate.setText(date[0]);
         holder.tvDayName.setText(viewSlotsModel.getDay());
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                notifyDataSetChanged();
+                selected_position = position;
                 selectedDate.onDateSelected(viewSlotsModel.date);
             }
         });
+
+        if (selected_position == position) {
+            holder.constraintChildCardView.setBackgroundColor(Color.parseColor("#e6e6e6"));
+        } else {
+            holder.constraintChildCardView.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+        }
 
     }
 
@@ -58,11 +70,13 @@ public class ViewSlotsAdapter extends RecyclerView.Adapter<ViewSlotsAdapter.MyVi
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView tvDate;
         TextView tvDayName;
+        ConstraintLayout constraintChildCardView;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             tvDate = itemView.findViewById(R.id.tv_date);
             tvDayName = itemView.findViewById(R.id.tv_day_name);
+            constraintChildCardView = itemView.findViewById(R.id.constraint_child_card_view);
         }
     }
 
